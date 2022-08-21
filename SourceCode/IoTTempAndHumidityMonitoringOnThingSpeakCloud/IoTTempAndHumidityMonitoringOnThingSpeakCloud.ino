a4dd6840779c927a5d74fff3111ccb6c05fbd7e8
Prateek <prateek.ideafires@gmail.com>
2022-08-21 15:35:29 +0530

    Updated Code ,there was an error in loop.

--- SourceCode/IoTTempAndHumidityMonitoringOnThingSpeakCloud/IoTTempAndHumidityMonitoringOnThingSpeakCloud.ino
+++ SourceCode/IoTTempAndHumidityMonitoringOnThingSpeakCloud/IoTTempAndHumidityMonitoringOnThingSpeakCloud.ino
@@ -1,103 +1,99 @@
-
 #include <ESP8266WiFi.h>
 #include <DHT.h>
 
-String apiKey = “92TE724P1VSWQNGK”; // Enter your Write API key from ThingSpeak
-
-const char *ssid = “Pinwheel”; // replace with your wifi ssid and wpa2 key
-const char *pass = “Magentalight”;
-const char* server = “api.thingspeak.com”;
+String apiKey = "92TE724P1VSWQNGK"; // Enter your Write API key from ThingSpeak
+const char *ssid = "Pinwheel"; // replace with your wifi ssid and wpa2 key
+const char *pass = "YourWiFiPassword";
+const char* server = "api.thingspeak.com";
 
 #define DHTPIN 0 //pin where the dht11 is connected
-const int relay = 4;
-DHT dht(DHTPIN, DHT11);
+#define DHTTYPE DHT11
+#define RELAY_PIN 4
+
+DHT dht(DHTPIN, DHTTYPE);
 
 WiFiClient client;
 
 void setup()
 {
-Serial.begin(115200);
-delay(10);
-dht.begin();
+  pinMode(RELAY_PIN,OUTPUT);
+  Serial.begin(9600);
+  delay(10);
+  dht.begin();
 
-Serial.println(“Connecting to “);
-Serial.println(ssid);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
 
-WiFi.begin(ssid, pass);
+  WiFi.begin(ssid, pass);
 
-while (WiFi.status() != WL_CONNECTED)
-{
-delay(500);
-Serial.print(“.”);
-}
-Serial.println(“”);
-Serial.println(“WiFi connected”);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 
 }
 
 void loop()
 {
 
-float h = dht.readHumidity();
-float t = dht.readTemperature();
-
-if (isnan(h) || isnan(t))
-{
-Serial.println(“Failed to read from DHT sensor!”);
-return;
-}
-
-if (client.connect(server,80)) // “184.106.153.149” or api.thingspeak.com
-{
-
-String postStr = apiKey;
-postStr +=”&field1=”;
-postStr += String(t);
-postStr +=”&field2=”;
-postStr += String(h);
-postStr += “\r\n\r\n”;
-
-client.print(“POST /update HTTP/1.1\n”);
-client.print(“Host: api.thingspeak.com\n”);
-client.print(“Connection: close\n”);
-client.print(“X-THINGSPEAKAPIKEY: “+apiKey+”\n”);
-client.print(“Content-Type: application/x-www-form-urlencoded\n”);
-client.print(“Content-Length: “);
-client.print(postStr.length());
-client.print(“\n\n”);
-client.print("postStr");
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
   
-Serial.print("Temperature:");
-Serial.print( "t") ;
-Serial.print("degrees Celcius, Humidity: ");
-Serial.print("h");
-Serial.println("Send to Thingspeak.");
-}
-void loop()
-{
-{
-
-/////////////////////////////////////// SEND THE QUERY AND RECEIVE THE RESPONSE///////////////////////
-
-if(t >= 500) //Check the sensor output
-{
-t=500; // to indicate on server that object is detected
-digitalWrite(relay, HIGH);
-digitalWrite(relay, HIGH); // switch on the relay
-Serial.println ("cooling on " ) 
-
-}
-else
-{
-t<=50; //
-digitalWrite(relay, LOW);
-digitalWrite(relay, LOW);
-Serial.println("temperature in limit " ) ;
-}
-client.stop();
-
-Serial.println("waiting..."); 
-
-// thingspeak needs minimum 15 sec delay between updates
-delay(1000);
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  if (client.connect(server,80)) // “184.106.153.149” or api.thingspeak.com
+  {
+  
+    String postStr = apiKey;
+    postStr +="&field1=";
+    postStr += String(temperature);
+    postStr +="&field2=";
+    postStr += String(humidity);
+    postStr += "\r\n\r\n";
+  
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+    
+    Serial.print("Temperature:");
+    Serial.print( temperature) ;
+    Serial.print("degrees Celcius, Humidity: ");
+    Serial.print(humidity);
+    Serial.println("Sent to Thingspeak.");
+    //Operating Relay Based on Temperature && Humidity
+    if(temperature >= 500) //Check the sensor output
+    {
+      temperature=500; // 
+      digitalWrite(RELAY_PIN, HIGH);
+      digitalWrite(RELAY_PIN, HIGH); // switch on the relay
+      Serial.println ("cooling on " );
+  
+    }
+    else
+    {
+      temperature<=50; //
+      digitalWrite(RELAY_PIN, LOW);
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("cooling off" );
+    }
+    client.stop();
+  
+    Serial.println("waiting..."); 
+  
+    //thingspeak needs minimum 15 sec delay between updates
+    delay(15000);
+  }
 }
